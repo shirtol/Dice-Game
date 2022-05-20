66bb017286faca2a1fd8a630abe80278e9eec3e6
shirtol <shirzahavi7@gmail.com>
2022-05-20 11:44:44 +0300

    Update player score

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -6,7 +6,6 @@ import "./Game.css";
 export default class Game extends Component {
     state = {
         totalScores: [0, 0],
-        currTurnScore: 0,
         activePlayer: 0,
         currScore: 0,
     };
@@ -17,19 +16,39 @@ export default class Game extends Component {
         }));
     };
 
+    onEndTurn = () => {
+        const newScoresArr = [...this.state.totalScores];
+        newScoresArr[this.state.activePlayer] += this.state.currScore;
+        this.setState((prevState) => ({
+            totalScores: newScoresArr,
+            currScore: 0,
+            activePlayer:
+                (prevState.activePlayer + 1) % prevState.totalScores.length,
+        }));
+    };
+
     render() {
         return (
             <div className="game-container">
                 <Player
                     name="player 1"
-                    totalScore="0"
-                    currScore={this.state.currScore}
+                    totalScore={this.state.totalScores[0]}
+                    currScore={
+                        this.state.activePlayer === 0 ? this.state.currScore : 0
+                    }
                 ></Player>
                 <GamePanel
                     range={[1, 6]}
                     onRollDice={this.onRollDice}
+                    onEndTurn={this.onEndTurn}
                 ></GamePanel>
-                <Player name="player 2" totalScore="0" currScore="0"></Player>
+                <Player
+                    name="player 2"
+                    totalScore={this.state.totalScores[1]}
+                    currScore={
+                        this.state.activePlayer === 1 ? this.state.currScore : 0
+                    }
+                ></Player>
             </div>
         );
     }
